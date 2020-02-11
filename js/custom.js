7f213b4b5e93b89fc7a00ce2b532b52fb5d618ea
eduardodallatorre.io <eduardodallatorre.io@gmail.com>
2020-02-11 11:52:16 -0300

    1update

--- js/custom.js
+++ js/custom.js
@@ -1,54 +1,36 @@
-/* HTML document is loaded. DOM is ready.
--------------------------------------------*/
-$(function(){
+var tags = [
+  "This website aims to show my portfolio and share my knowledge about Design and Programming.",
+  "Come be part of this team, through my social networks.",
+  "My mission is to add knowledge."
+]
 
-    /* start typed element */
-    //http://stackoverflow.com/questions/24874797/select-div-title-text-and-make-array-with-jquery
-    var subElementArray = $.map($('.sub-element'), function(el) { return $(el).text(); });    
-    $(".element").typed({
-        strings: subElementArray,
-        typeSpeed: 30,
-        contentType: 'html',
-        showCursor: false,
-        loop: true,
-        loopCount: true,
-    });
-    /* end typed element */
-
-    /* Smooth scroll and Scroll spy (https://github.com/ChrisWojcik/single-page-nav)    
-    ---------------------------------------------------------------------------------*/ 
-    $('.templatemo-nav').singlePageNav({
-        offset: $(".templatemo-nav").height(),
-        filter: ':not(.external)',
-        updateHash: false
-    });
-
-    /* start navigation top js */
-    $(window).scroll(function(){
-        if($(this).scrollTop()>58){
-            $(".templatemo-nav").addClass("sticky");
-        }
-        else{
-            $(".templatemo-nav").removeClass("sticky");
-        }
-    });
-    
-    /* Hide mobile menu after clicking on a link
-    -----------------------------------------------*/
-    $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
-    /* end navigation top js */
-
-    $('body').bind('touchstart', function() {});
-
-    /* wow
-    -----------------*/
-    new WOW().init();
+$(document).ready(function(){
+  setTimeout(backspaceTagContent, 1000);
 });
 
-/* start preloader */
-$(window).load(function(){
-	$('.preloader').fadeOut(1000); // set duration in brackets    
-});
-/* end preloader */
+function changeTagContent() {
+  backspaceTagContent();
+}
+
+function backspaceTagContent() {
+  $('#tag-content').html($('#tag-content').html().slice(0,-1));
+  if ($('#tag-content').html().length) {
+    setTimeout(backspaceTagContent, 50);
+  } else {
+    setTimeout(function() { insertTagContent(getRandomTag()); }, 100);
+  }
+}
+
+function insertTagContent(tag) {
+  $('#tag-content').html(tag.substring(0, $('#tag-content').html().length + 1));
+  if ($('#tag-content').html().length != tag.length) {
+    setTimeout(function() { insertTagContent(tag); }, 100);
+  } else {
+    setTimeout(changeTagContent, 5000);
+  }
+}
+
+function getRandomTag() {
+  return tags[Math.floor(Math.random() * tags.length)];
+  
+}
